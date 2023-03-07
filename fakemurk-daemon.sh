9cea33b6f6cf6b5c5ba9595292de065ad01f0bb8
CoolElectronics <58010778+CoolElectronics@users.noreply.github.com>
2023-03-06 19:36:41 -0500

    somehow i accidentally deleted autoupdater???

--- fakemurk-daemon.sh
+++ fakemurk-daemon.sh
@@ -8,6 +8,7 @@
     {
         while true; do
             cryptohome --action=remove_firmware_management_parameters >/dev/null 2>&1
+            sleep 0.1
         done
     } &
     {
@@ -22,6 +23,23 @@
     while true; do
         if test -d "/home/chronos/user/Downloads/disable-extensions"; then
             kill -9 $(pgrep -f "\-\-extension\-process") 2>/dev/null
+            sleep 0.5
+        else
+            sleep 5
         fi
+    done
+} &
+{
+    while true; do
+        if ! [ -f /mnt/stateful_partition/fakemurk_version ]; then
+            echo -n "CURRENT_VERSION=0" >/mnt/stateful_partition/fakemurk_version
+        fi
+        . /mnt/stateful_partition/fakemurk_version
+        . <(curl https://github.com/MercuryWorkshop/fakemurk/blob/main/autoupdate.sh)
+        if ((UPDATE_VERSION > CURRENT_VERSION)); then
+            echo -n "CURRENT_VERSION=$UPDATE_VERSION" >/mnt/stateful_partition/fakemurk_version
+            autoupdate
+        fi
+        sleep 20m
     done
 } &
