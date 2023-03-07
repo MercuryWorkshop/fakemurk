7f4bf9e7dc22280b097aabab428133848f3d5d57
CoolElectronics <58010778+CoolElectronics@users.noreply.github.com>
2023-03-06 19:28:27 -0500

    fix the chromebook bricking if you happen to not have wifi

---
+++ autoupdate.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+UPDATE_VERSION=1
+get_asset() {
+    curl -s -f "https://api.github.com/repos/MercuryWorkshop/fakemurk/contents/$1" | jq -r ".content" | base64 -d
+}
+install() {
+    TMP=$(mktemp)
+    get_asset "$1" >"$TMP"
+    if [ "$?" == "1" ] || ! grep -q '[^[:space:]]' "$TMP"; then
+        echo "failed to install $1 to $2"
+        # rm -f "$TMP"
+        return 1
+    fi
+    # don't mv, as that would break permissions i spent so long setting up
+    cat "$TMP" >"$2"
+    rm -f "$TMP"
+}
+
+update_files() {
+    install "fakemurk-daemon.sh" /sbin/fakemurk-daemon.sh
+    install "chromeos_startup.sh" /sbin/chromeos_startup.sh
+    install "mush.sh" /usr/bin/crosh
+    install "pre-startup.conf" /etc/init/pre-startup.conf
+}
+
+autoupdate() {
+    update_files
+}
+
+if [ "$0" = "$BASH_SOURCE" ]; then
+    autoupdate
+fi
