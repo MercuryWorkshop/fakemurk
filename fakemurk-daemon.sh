65b357890e5a65e24ab230cb4bab779003cdb453
CoolElectronics <58010778+CoolElectronics@users.noreply.github.com>
2023-03-11 20:56:49 -0500

    olyb, we overwrite crossystem, that's like the entire basis of this project

--- fakemurk-daemon.sh
+++ fakemurk-daemon.sh
@@ -40,7 +40,7 @@
         while true; do
             vpd -i RW_VPD -s check_enrollment=0 >/dev/null 2>&1
             vpd -i RW_VPD -s block_devmode=0 >/dev/null 2>&1
-            crossystem block_devmode=0 >/dev/null 2>&1
+            crossystem.old block_devmode=0 >/dev/null 2>&1
             sleep 60
         done
     } &
