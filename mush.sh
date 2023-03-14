b55573d19a685c4b4d93bc37b77f904cb44b114b
rainestorme <115757568+rainestorme@users.noreply.github.com>
2023-03-14 15:09:05 -0500

    Improve mush

--- mush.sh
+++ mush.sh
@@ -19,7 +19,7 @@ get_largest_nvme_namespace() {
 traps() {
     set +e
     trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
-    trap 'echo "\"${last_command}\" command failed with exit code $?. THIS IS A BUG, REPORT IT HERE https://github.com/MercuryWorkshop/fakemurk"' EXIT
+    trap 'echo "\"${last_command}\" command failed with exit code $?' EXIT
     trap '' INT
 }
 
@@ -29,7 +29,7 @@ Welcome to mush, the fakemurk developer shell.
 
 If you got here by mistake, don't panic! Just close this tab and carry on.
 
-This shell contains a list of utilities for performing certain actions on a fakemurked chromebook
+This shell contains a list of utilities for performing various actions on a fakemurked chromebook.
 
 EOF
 
@@ -82,12 +82,14 @@ main() {
 (7) Hard Enable Extensions
 (8) Emergency Revert & Re-Enroll
 (9) Edit Pollen
+(10) Run neofetch
 EOF
         if ! test -f /mnt/stateful_partition/crouton; then
-            echo "(10) Install Crouton"
+            echo "(11) Install Crouton"
+            echo "(12) Start Crouton (only run after running above)"
         fi
         swallow_stdin
-        read -r -p "> (1-10): " choice
+        read -r -p "> (1-12): " choice
         case "$choice" in
         1) runjob doas bash ;;
         2) runjob bash ;;
@@ -98,16 +100,22 @@ EOF
         7) runjob hardenableext ;;
         8) runjob revert ;;
         9) runjob edit /etc/opt/chrome/policies/managed/policy.json ;;
-        10) runjob install_crouton ;;
-        *) echo "invalid option" ;;
+        10) runjob do_neofetch ;;
+        11) runjob install_crouton ;;
+        12) runjob run_crouton ;;
+        *) echo "----- Invalid option ------" ;;
         esac
     done
 }
 
+do_neofetch() {
+    curl https://raw.githubusercontent.com/dylanaraps/neofetch/master/neofetch | bash
+}
+
 powerwash() {
-    echo "ARE YOU SURE YOU WANT TO POWERWASH??? THIS WILL REMOVE ALL USER ACCOUNTS"
+    echo "Are you sure you wanna powerwash? This will remove all user accounts and data, but won't remove fakemurk."
     sleep 2
-    echo "(press enter to continue, ctrl-c to cancel)"
+    echo "(Press enter to continue, ctrl-c to cancel)"
     swallow_stdin
     read -r
     doas echo "fast safe" >/mnt/stateful_partition/factory_install_reset
@@ -117,12 +125,12 @@ powerwash() {
 
 revert() {
     echo "This option will re-enroll your chromebook restore to before fakemurk was run. This is useful if you need to quickly go back to normal"
-    echo "THIS IS A PERMANENT CHANGE!! YOU WILL NOT BE ABLE TO GO BACK UNLESS YOU UNENROLL AGAIN AND RUN THE SCRIPT, AND IF YOU UPDATE TO THE VERSION SH1MMER IS PATCHED, YOU MAY BE STUCK ENROLLED"
-    echo "ARE YOU SURE YOU WANT TO CONTINUE? (press enter to continue, ctrl-c to cancel)"
+    echo "This is *permanent*. You will not be able to fakemurk again unless you re-run everything from the beginning."
+    echo "Are you sure - 100% sure - that you want to continue? (press enter to continue, ctrl-c to cancel)"
     swallow_stdin
     read -r
     sleep 4
-    echo "setting kernel priority"
+    echo "Setting kernel priority"
 
     DST=/dev/$(get_largest_nvme_namespace)
 
@@ -133,7 +141,7 @@ revert() {
         doas cgpt add "$DST" -i 4 -P 0
         doas cgpt add "$DST" -i 2 -P 1
     fi
-    echo "setting vpd"
+    echo "Setting vpd"
     doas vpd.old -i RW_VPD -s check_enrollment=1
     doas vpd.old -i RW_VPD -s block_devmode=1
     doas crossystem.old block_devmode=1
@@ -141,19 +149,19 @@ revert() {
     echo "Done. Press enter to reboot"
     swallow_stdin
     read -r
-    echo "bye!"
+    echo "Bye!"
     sleep 2
     doas reboot
     sleep 1000
 }
 harddisableext() { # calling it "hard disable" because it only reenables when you press
-    read -r -p "enter extension id>" extid
+    read -r -p "Enter extension ID > " extid
     chmod 000 "/home/chronos/user/Extensions/$extid"
     kill -9 $(pgrep -f "\-\-extension\-process")
 }
 
 hardenableext() {
-    read -r -p "enter extension id>" extid
+    read -r -p "Enter extension ID > " extid
     chmod 777 "/home/chronos/user/Extensions/$extid"
     kill -9 $(pgrep -f "\-\-extension\-process")
 }
@@ -166,7 +174,12 @@ softdisableext() {
     done
 }
 install_crouton() {
+    echo "Installing Crouton on /mnt/stateful_partition"
     doas "bash <(curl -SLk https://goo.gl/fd3zc) -t xfce -r bullseye" && touch /mnt/stateful_partition/crouton
+}
+run_crouton() {
+    echo "Use Crtl+Shift+Alt+Forward and Ctrl+Shift+Alt+Back to toggle between desktops"
+    doas "startxfce4"
 }
 if [ "$0" = "$BASH_SOURCE" ]; then
     stty sane
